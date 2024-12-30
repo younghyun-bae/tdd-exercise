af92498db82301589e3f1cc870d6347cecbcfd74
Younghyun <yhbae97@gmail.com>
2024-12-30 21:38:43 +0100

    Add a simple Mock challenge and Sort out files

--- README.md
+++ README.md
@@ -20,3 +20,7 @@ jest --coverage
 ```
 
 ### Calulator Testing
+
+### Async Testing
+
+### Mocking
--- src/basic/test/add.test.js
+++ src/basic/test/add.test.js
@@ -1,4 +1,4 @@
-const add = require('../add.js');
+const add = require('../../add.js');
 
 test('add', () => {
   expect(add(1, 2)).toBe(3);
