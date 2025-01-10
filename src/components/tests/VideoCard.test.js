6a0a094e594b8894a94fc7994c0e61dba3936af8
Younghyun <yhbae97@gmail.com>
2025-01-10 19:56:17 +0100

    test: Add unit testing for navigate response

--- src/components/tests/VideoCard.test.js
+++ src/components/tests/VideoCard.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter, Route, Routes, useLocation } from 'react-router-dom';
 import { formatAgo } from '../../util/date';
 import VideoCard from '../VideoCard';
 
@@ -18,6 +19,7 @@ describe('VideoCard', () => {
       },
     },
   };
+
   const { title, channelTitle, publishedAt, thumbnails } = video.snippet;
 
   it('renders video item', () => {
@@ -33,5 +35,28 @@ describe('VideoCard', () => {
     expect(screen.getByText(title)).toBeInTheDocument();
     expect(screen.getByText(channelTitle)).toBeInTheDocument();
     expect(screen.getByText(formatAgo(publishedAt))).toBeInTheDocument();
+  });
+
+  it('navigates to detailed video page with video state when clicked', async () => {
+    function LocationStateDisplay() {
+      const location = useLocation();
+      return <pre>{JSON.stringify(location.state, null, 2)}</pre>;
+    }
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <Routes>
+          <Route path='/' element={<VideoCard video={video} />} />
+          <Route 
+            path={`/videos/watch/${video.id}`} 
+            element={<LocationStateDisplay />}
+          />
+        </Routes>
+      </MemoryRouter>    
+    );
+    const card = screen.getByRole('listitem');
+    userEvent.click(card);
+
+    await screen.findByText(/"id": 1/);
+    expect(screen.getByText(/"title": "title"/)).toBeInTheDocument();
   });
 });
